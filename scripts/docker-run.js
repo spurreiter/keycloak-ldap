6ce5362d486fc27384f9b5cf533bb4ab6e9ead96
spurreiter <spurreiter@users.noreply.github.com>
2020-10-20 08:41:36 +0200

    chore: consider different os

--- scripts/docker-run.js
+++ scripts/docker-run.js
@@ -6,15 +6,29 @@ const { execSync } = require('child_process')
 const config = require('./config.js')
 const execOpts = { stdio: 'inherit' }
 
-function rewriteRealmForDarwin () {
+const isMacOs = () => os.platform() === 'darwin'
+
+function rewriteRealm () {
   const filename = `${__dirname}/my-realm.json`
   const realm = require(filename)
-  realm.components['org.keycloak.storage.UserStorageProvider'][0].config.connectionUrl[0] = 'ldap://host.internal.docker:1389'
+  realm.components['org.keycloak.storage.UserStorageProvider'][0].config.connectionUrl[0] = isMacOs()
+    ? 'ldap://host.docker.internal:1389'
+    : 'ldap://localhost:1389'
+  realm.smtpServer.host = isMacOs()
+    ? 'host.docker.internal'
+    : 'localhost'
+
   fs.writeFileSync(filename, JSON.stringify(realm, null, 2), 'utf8')
 }
 
 // @see https://registry.hub.docker.com/r/jboss/keycloak
 async function dockerRun () {
+  const startArgs = [
+    '-Dkeycloak.profile=preview'
+  ].join(' s')
+
+  rewriteRealm()
+
   let cmd = [
     'docker run -d',
     '--name keycloak',
@@ -25,18 +39,17 @@ async function dockerRun () {
     `-v ${__dirname}/my-realm.json:/tmp/my-realm.json`
   ]
 
-  if (os.platform() === 'darwin') {
-    rewriteRealmForDarwin()
+  if (isMacOs()) {
     cmd = cmd.concat([
       '-p 8080:8080',
       `my/keycloak:${config.keycloakVersion}`,
-      '/opt/jboss/tools/docker-entrypoint.sh -b 0.0.0.0'
+      `/opt/jboss/tools/docker-entrypoint.sh -b 0.0.0.0 ${startArgs}`
     ])
   } else {
     cmd = cmd.concat([
       '--network host', // needed for linux
       `my/keycloak:${config.keycloakVersion}`,
-      '/opt/jboss/tools/docker-entrypoint.sh -c standalone.xml -b 127.0.0.1' // needed for linux
+      `/opt/jboss/tools/docker-entrypoint.sh -c standalone.xml -b 127.0.0.1 ${startArgs}` // needed for linux
     ])
   }
 
