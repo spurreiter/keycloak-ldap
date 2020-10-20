4d87c92ee145b4f5707489f4d882721236ebb02b
spurreiter <spurreiter@users.noreply.github.com>
2020-10-20 08:41:17 +0200

    chore: run dev

--- scripts/docker-run.js
+++ scripts/docker-run.js
@@ -1,24 +1,46 @@
 #!/usr/bin/env node
 
+const os = require('os')
+const fs = require('fs')
 const { execSync } = require('child_process')
 const config = require('./config.js')
 const execOpts = { stdio: 'inherit' }
 
+function rewriteRealmForDarwin () {
+  const filename = `${__dirname}/my-realm.json`
+  const realm = require(filename)
+  realm.components['org.keycloak.storage.UserStorageProvider'][0].config.connectionUrl[0] = 'ldap://host.internal.docker:1389'
+  fs.writeFileSync(filename, JSON.stringify(realm, null, 2), 'utf8')
+}
+
 // @see https://registry.hub.docker.com/r/jboss/keycloak
 async function dockerRun () {
-  execSync([
+  let cmd = [
     'docker run -d',
     '--name keycloak',
     '-h keycloak',
     `-e KEYCLOAK_USER=${config.username}`,
     `-e KEYCLOAK_PASSWORD=${config.password}`,
     '-e KEYCLOAK_IMPORT=/tmp/my-realm.json',
-    `-v ${__dirname}/my-realm.json:/tmp/my-realm.json`,
-    // '-p 8080:8080',
-    '--network host', // needed for linux
-    `my/keycloak:${config.keycloakVersion}`,
-    '/opt/jboss/tools/docker-entrypoint.sh -c standalone.xml -b 127.0.0.1' // needed for linux
-  ].join(' '), execOpts)
+    `-v ${__dirname}/my-realm.json:/tmp/my-realm.json`
+  ]
+
+  if (os.platform() === 'darwin') {
+    rewriteRealmForDarwin()
+    cmd = cmd.concat([
+      '-p 8080:8080',
+      `my/keycloak:${config.keycloakVersion}`,
+      '/opt/jboss/tools/docker-entrypoint.sh -b 0.0.0.0'
+    ])
+  } else {
+    cmd = cmd.concat([
+      '--network host', // needed for linux
+      `my/keycloak:${config.keycloakVersion}`,
+      '/opt/jboss/tools/docker-entrypoint.sh -c standalone.xml -b 127.0.0.1' // needed for linux
+    ])
+  }
+
+  execSync(cmd.join(' '), execOpts)
 }
 
 dockerRun()
--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ const { Adapter } = require('./adapter/index.js')
 const { ldapServer } = require('./ldapServer.js')
 
 module.exports = {
+  Adapter,
   ldapServer
-}
-
-if (module === require.main) {
-  require('../scripts/client.js')
-  require('../scripts/mail-server.js')
-
-  const config = {
-    port: 1389,
-    bindDN: 'cn=Administrator,cn=Users,dc=example,dc=local',
-    bindPassword: 'ldap-password',
-    suffix: 'cn=Users,dc=example,dc=local'
-  }
-
-  const adapter = new Adapter(config)
-
-  const server = ldapServer(config, adapter)
-
-  server.listen(config.port, () => {
-    var host = server.address().address
-    var port = server.address().port
-    console.log('LDAP on ldap://%s:%s', host, port)
-  })
 }
